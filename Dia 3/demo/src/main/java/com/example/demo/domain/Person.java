a3efced2dae2a69ee13d54ef2d37489d8f4b48d5
Alejandra2070 <am3793825@gmail.com>
2025-04-25 13:00:23 -0500

    :sparkles: Code in class

---
+++ Dia 3/demo/src/main/java/com/example/demo/domain/Person.java
@@ -0,0 +1,65 @@
+package com.example.demo.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity //crear una entidad
+@Table(name = "personas") //crear tabla
+public class Person {
+    
+    @Id //llave primaria
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //generar automaticamente
+    private Long id;
+
+    private String name;
+    private String lastName;
+
+    @Column(name = "programming_language") //renombrar una columna
+    private String language;
+
+    public Person() {
+    }
+
+    public Person(Long id, String name, String lastName, String language) {
+        this.id = id;
+        this.name = name;
+        this.lastName = lastName;
+        this.language = language;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+}
